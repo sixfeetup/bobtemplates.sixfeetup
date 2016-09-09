0f63aa057b8de047c8c5dce68006d2e3c67abfea
Calvin Hendryx-Parker <calvin@sixfeetup.com>
2016-09-09 17:38:37 -0400

    updating hooks to work with new structure

--- TODO.rst
+++ TODO.rst
@@ -1,9 +1,6 @@
 TODO
 ====
 
-- make computed variable for pre_plone4 available to the templates
-- make computed variable for pyflakes_version (pre4 needs pyflakes<=0.4)
-- make computed variable for plone_major_version
 - add question for License and update setup.py.bob
 - make option for plone 5 registries replacement of props tool
   - properties.xml
@@ -12,3 +9,4 @@ TODO
   - email addreses in site props
   - sixfeetup.utils (publish on PyPI?)
 - remove dep in theme profile for p.a.themeing on plone >= 5
+- test offline mode (extends cache might not be working)
--- bobtemplates/sixfeetup/hooks.py
+++ bobtemplates/sixfeetup/hooks.py
@@ -3,13 +3,8 @@
 """Render bobtemplates.sixfeetup hooks.
 """
 from datetime import date
-from mrbob.bobexceptions import SkipQuestion
-from mrbob.bobexceptions import ValidationError
-from mrbob.hooks import validate_choices
 
-import keyword
 import os
-import re
 import shutil
 import string
 import subprocess
@@ -41,10 +36,6 @@ def validate_projectname(configurator):
     if package_dir.startswith('.') or package_dir.endswith('.'):
         fail = True
 
-    parts = len(package_dir.split('.'))
-    if parts < 2 or parts > 3:
-        fail = True
-
     if fail:
         msg = "Error: '{0}' is not a valid project name.\n".format(package_dir)
         msg += "Please use a valid name (like mybuildout or "
@@ -52,13 +43,24 @@ def validate_projectname(configurator):
         sys.exit(msg)
 
 
+def pre_site_name(configurator, question):
+    """ grab the target directory to create project name
+    """
+    validate_projectname(configurator)
+
+
 def pre_username(configurator, question):
     """Get email from git and validate package name.
     """
-    # validate_packagename should be run before asking the first question.
-    validate_packagename(configurator)
+    default = get_git_info('github.user')
+    if default:
+        question.default = default
 
-    default = get_git_info('user_name')
+
+def pre_name(configurator, question):
+    """Get name from git and validate package name.
+    """
+    default = get_git_info('user.name')
     if default:
         question.default = default
 
@@ -66,186 +68,88 @@ def pre_username(configurator, question):
 def pre_email(configurator, question):
     """Get email from git.
     """
-    default = get_git_info('user_email')
+    default = get_git_info('user.email')
     if default:
         question.default = default
 
 
 def post_plone_version(configurator, question, answer):
-    """Find out if it is supposed to be Plone 5.
+    """Find out what version of Plone we want
     """
     _set_plone_version_variables(configurator, answer)
     return answer
 
 
 def _set_plone_version_variables(configurator, version):
-    if 'plone.is_plone5' not in configurator.variables:
-        # Find out if it is supposed to be Plone 5.
-        if version.startswith('5'):
-            configurator.variables['plone.is_plone5'] = True
-        else:
-            configurator.variables['plone.is_plone5'] = False
-    if 'plone.minor_version' not in configurator.variables:
-        # extract minor version (4.3)
-        # (according to https://plone.org/support/version-support-policy)
-        # this is used for the trove classifier in setup.py of the product
-        configurator.variables['plone.minor_version'] = '.'.join(
-            version.split('.')[:2])
+    configurator.variables['is_plone5'] = False
+    configurator.variables['is_plone4'] = False
+    configurator.variables['pre_plone4'] = False
+    configurator.variables['pyflakes_version'] = 'pyflakes'
+    # Find out if it is supposed to be Plone 5.
+    if version.startswith('5'):
+        configurator.variables['is_plone5'] = True
+    if version.startswith('4'):
+        configurator.variables['is_plone4'] = True
+    else:
+        configurator.variables['pre_plone4'] = True
+        configurator.variables['pyflakes_version'] = 'pyflakes<=0.4'
+    configurator.variables['plone_minor_version'] = ".".join(
+        version.split('.')[:2])
 
 
-def post_ask(configurator):
-    """Make sure some variables are set, also in non-interactive mode.
-
-    This is called after all questions have been asked.
+def post_password(configurator, question, answer):
+    """ generate password if pwgen is installed and no password provided
     """
-    configurator.variables['year'] = date.today().year
-    version = configurator.variables.get('plone.version')
-    if not version:
-        return
-    _set_plone_version_variables(configurator, version)
+    if not answer:
+        answer = run_cmd('pwgen -acn 9 1')
+        if not answer:
+            answer = 'admin'
+    return answer
+
+
+def run_cmd(cmd):
+    """Run a command and get back the output
+    """
+    ret = subprocess.Popen(
+        cmd,
+        shell=True,
+        stdout=subprocess.PIPE
+        ).communicate()
+    return ret[0][:-1]
 
 
 def prepare_render(configurator):
-    """Some variables to make templating easier.
-
-    This is especially important for allowing nested and normal packages.
+    """Set the project_name for the buildout
     """
     # get package-name and package-type from user-input
-    package_dir = os.path.basename(configurator.target_directory)
-    nested = bool(len(package_dir.split('.')) == 3)
-    configurator.variables['package.nested'] = nested
-    configurator.variables['package.namespace'] = package_dir.split('.')[0]
-    if nested:
-        namespace2 = package_dir.split('.')[1]
-    else:
-        namespace2 = None
-    configurator.variables['package.namespace2'] = namespace2
-    configurator.variables['package.name'] = package_dir.split('.')[-1]
-
-    if nested:
-        dottedname = "{0}.{1}.{2}".format(
-            configurator.variables['package.namespace'],
-            configurator.variables['package.namespace2'],
-            configurator.variables['package.name'])
-    else:
-        dottedname = "{0}.{1}".format(
-            configurator.variables['package.namespace'],
-            configurator.variables['package.name'])
-
-    # package.dottedname = 'collective.foo.something'
-    configurator.variables['package.dottedname'] = dottedname
-
-    # package.uppercasename = 'COLLECTIVE_FOO_SOMETHING'
-    configurator.variables['package.uppercasename'] = configurator.variables[
-        'package.dottedname'
-    ].replace('.', '_').upper()
-
-    camelcasename = dottedname.replace('.', ' ').title()\
-        .replace(' ', '')\
-        .replace('_', '')
-    browserlayer = "{0}Layer".format(camelcasename)
-
-    # package.browserlayer = 'CollectiveFooSomethingLayer'
-    configurator.variables['package.browserlayer'] = browserlayer
-
-    # package.longname = 'collectivefoosomething'
-    configurator.variables['package.longname'] = camelcasename.lower()
-
-    # jenkins.directories = 'collective/foo/something'
-    configurator.variables[
-        'jenkins.directories'
-    ] = dottedname.replace('.', '/')
-
-    # namespace_packages = "['collective', 'collective.foo']"
-    if nested:
-        namespace_packages = "'{0}', '{0}.{1}'".format(
-            configurator.variables['package.namespace'],
-            configurator.variables['package.namespace2'])
-    else:
-        namespace_packages = "'{0}'".format(
-            configurator.variables['package.namespace'])
-    configurator.variables['package.namespace_packages'] = namespace_packages
-
-    if configurator.variables.get('package.dexterity_type_name'):
-        configurator.variables[
-            'package.dexterity_type_name_lower'
-        ] = configurator.variables['package.dexterity_type_name'].lower()
-    else:
-        # We have to make sure those variables are always set because we are
-        # going to create files that contain those variables. Even if we
-        # remove those files afterwards. This is just how mr.bob rolls.
-        configurator.variables['package.dexterity_type_name'] = ''
-        configurator.variables['package.dexterity_type_name_lower'] = ''
+    project_name = os.path.basename(configurator.target_directory)
+    configurator.variables['project_name'] = project_name
 
 
 def cleanup_package(configurator):
-    """Cleanup and make nested if needed.
-
-    Transform into a nested package if that was the selected option.
-    Remove parts that are not needed depending on the chosen configuration.
+    """Removed unneeded bits based on answers to question
     """
-
-    nested = configurator.variables['package.nested']
-
-    # construct full path '.../src/collective'
-    start_path = "{0}/src/{1}".format(
-        configurator.target_directory,
-        configurator.variables['package.namespace'])
-
-    # path for normal packages: '.../src/collective/myaddon'
+    # path for normal packages in a unified buildut (project/project/content)
     base_path = "{0}/{1}".format(
-        start_path,
-        configurator.variables['package.name'])
-
-    if nested:
-        # Event though the target-dir was 'collective.behavior.myaddon' mrbob
-        # created a package collective.behavior.myaddon/src/collective/myaddon
-        # since the template does not hava a folder for namespace2.
-        # Here this package is turned into a nested package
-        # collective.behavior.myaddon/src/collective/behavior/myaddon by
-        # inserting a folder with the namepsace2 ('behavior') and oopying
-        # a __init__.py into it.
-
-        # full path for nested packages: '.../src/collective/behavior/myaddon'
-        base_path_nested = "{0}/{1}/{2}".format(
-            start_path,
-            configurator.variables['package.namespace2'],
-            configurator.variables['package.name'])
-
-        # directory to be created: .../src/collective/behavior
-        newpath = "{0}/{1}".format(
-            start_path,
-            configurator.variables['package.namespace2'])
-        if not os.path.exists(newpath):
-            # create new directory .../src/collective/behavior
-            os.makedirs(newpath)
-
-        # copy .../src/collective/__init__.py to
-        # .../src/collective/myaddon/__init__.py
-        shutil.copy2("{0}/__init__.py".format(start_path), newpath)
-
-        # move .../src/collective/myaddon to .../src/collective/behavior
-        shutil.move(base_path, base_path_nested)
-
-        # use the new path for deleting
-        base_path = base_path_nested
+        configurator.variables['project_name'],
+        configurator.variables['project_name'])
 
     # find out what to delete
     to_delete = []
 
-    if configurator.variables['package.type'] != u'Theme':
+    if not configurator.variables['include_policy']:
         to_delete.extend([
-            "{0}/theme",
-            "{0}/profiles/default/theme.xml",
-            "{0}/profiles/uninstall/theme.xml",
+            "{0}/policy",
         ])
 
-    if configurator.variables['package.type'] != u'Dexterity':
+    if not configurator.variables['include_theme']:
         to_delete.extend([
-            "{0}/profiles/default/types.xml",
-            "{0}/profiles/default/types",
-            "{0}/tests/test_.py",
-            "{0}/tests/robot/test_.robot",
+            "{0}/theme",
+        ])
+
+    if not configurator.variables['include_content']:
+        to_delete.extend([
+            "{0}/content",
         ])
 
     # remove parts
