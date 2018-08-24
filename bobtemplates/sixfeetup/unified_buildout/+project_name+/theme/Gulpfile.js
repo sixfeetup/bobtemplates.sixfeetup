a1ce29fd16aafe1854be6d5dfb7600812ec8c182
Anthony Bosio <anthony@sixfeetup.com>
2018-08-24 13:54:53 -0400

    add theme last_compilation timestamp update to gulp

--- bobtemplates/sixfeetup/unified_buildout/+project_name+/theme/Gulpfile.js
+++ bobtemplates/sixfeetup/unified_buildout/+project_name+/theme/Gulpfile.js
@@ -29,16 +29,37 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./diazo_resources/static/dist'));
 });
 
+gulp.task('registry', function() {
+    var date = new Date();
+    var year = date.getFullYear();
+    function zeroPad(item) {
+        if (item < 10) {
+            return "0" + item;
+        }
+        return item;
+    }
+    var month = zeroPad(date.getMonth() + 1);
+    var day = zeroPad(date.getDate());
+    var hours = zeroPad(date.getHours());
+    var minutes = zeroPad(date.getMinutes());
+    var seconds = zeroPad(date.getSeconds());
+    var timestamp = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+    gulp.src("./profiles/default/registry.xml")
+    .pipe(xmlTransformer([
+        { path: "//value[@key='last_compilation']", text: timestamp },
+    ]))
+    .pipe(gulp.dest("./profiles/default/"));
+});
+
 
 gulp.task('watch', function() {
     function informUser (event) {
         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
     }
-    var css_watcher = gulp.watch('./diazo_resources/static/css/*.less', ['styles']);
+    var css_watcher = gulp.watch('./diazo_resources/static/css/*.less', ['styles', 'registry']);
     css_watcher.on('change', informUser);
-    var js_watcher = gulp.watch('./diazo_resources/static/js/*.js', ['scripts']);
+    var js_watcher = gulp.watch('./diazo_resources/static/js/*.js', ['scripts', 'registry']);
     js_watcher.on('change', informUser);
 });
 
-gulp.task('build', ['styles', 'scripts']);
-gulp.task('default', ['watch']);
+gulp.task('build', ['styles', 'scripts', 'registry']);gulp.task('default', ['watch']);
